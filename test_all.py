dfb76f1f07bf62db17c15415a71fb3a66d31f0cb
U361220_ibe <nbou@iberdrola.com>
2024-05-07 14:28:38 +0300

    Some updates

--- test_all.py
+++ test_all.py
@@ -1,11 +1,17 @@
 from os import system
 from pathlib import Path as P
 from time import sleep
+from pprint import pprint
 
-test_folders = list(P("./training/").iterdir())
+with open("./best_f1.txt", "r") as f:
+    best_f1 = f.read().split("\n")[:-1]
+    
+test_folders = [P("./training") / bf for bf in best_f1]
+assert all([tf.exists() for tf in test_folders]), "Some test folders do not exist."
+
 for i, tf in enumerate(test_folders):
     run = tf.name
-    c = f"python test.py -m .\\{tf}\\weights\\best.pt -y .\\data.yaml -p testing -s test -b 256 -si -n {tf.name} -d 0,1,2,3"
+    c = f"python test.py -m .\\{tf}\\weights\\best.pt -y .\\data.yaml -p testing -s test -b 256 -si -n {run} -d 0,1,2,3"
     print(f"================ Testing run {i+1} named {run} ==================")
     system(c)
     print()
